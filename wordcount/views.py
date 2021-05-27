221c2064c23fc7c23e93056086412b6ccb54678e
Himanshu Itoria <itoriahimanshu@gmail.com>
2021-05-28 00:56:01 +0530

    First commit

---
+++ wordcount/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+
+
+def home(request):
+    return render(request,'home.html')
+
+def about(request):
+    return HttpResponse("Hey whats up")
+
+def count(request):
+    fulltxt = request.GET['fulltext']
+    wordlist = fulltxt.split()
+
+    worddictionary = {}
+    for word in wordlist :
+        if word in worddictionary:
+             worddictionary[word]+=1
+        else :
+             worddictionary[word]=1
+    sorteddic = sorted(worddictionary.items(), key=operator.itemgetter(1),reverse = True)
+
+    return render(request,'count.html',{'fulltxt':fulltxt, 'countit':len(wordlist),
+                                         'worddictionary':sorteddic})
